108c37d1024273c4abb986ef0f069dd199c10aff
Stefan Mehnert <StefanMehnert@gmail.com>
2017-01-01 13:51:33 +0100

    Request authorization to use the devices currents location. Zooms the camera to the current position

--- GeofencingTest/ViewController.swift
+++ GeofencingTest/ViewController.swift
@@ -7,21 +7,31 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    fileprivate let locationManager = CLLocationManager()
+    
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        guard let lastLocation = locations.last else { return }
+        
+        mapView.setCenter(lastLocation.coordinate, animated: true)
     }
-
-
 }
 
