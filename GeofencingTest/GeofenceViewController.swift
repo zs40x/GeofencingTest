05cc4b1b622cc3c0a72c052f0cb7facccae7aba7
Stefan Mehnert <StefanMehnert@gmail.com>
2017-01-02 15:31:04 +0100

    Added new, empty ViewController GeofenceViewController

---
+++ GeofencingTest/GeofenceViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GeofenceViewController.swift
+//  GeofencingTest
+//
+//  Created by Stefan Mehnert on 02/01/2017.
+//  Copyright © 2017 Stefan Mehnert. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GeofenceViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
