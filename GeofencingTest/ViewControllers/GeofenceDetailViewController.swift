b451b5eec1bd2a01c5a691cb43879ca3cdb024ea
Stefan Mehnert <StefanMehnert@gmail.com>
2017-01-03 19:35:12 +0100

    Renamed GeofenceViewController to GeofenceDetailViewController. Added working outlets and actions

---
+++ GeofencingTest/ViewControllers/GeofenceDetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  GeofenceViewController.swift
+//  GeofencingTest
+//
+//  Created by Stefan Mehnert on 03/01/2017.
+//  Copyright © 2017 Stefan Mehnert. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class GeofenceDetailViewController: UIViewController {
+
+    @IBOutlet weak var labelRadius: UILabel!
+    @IBOutlet weak var sliderRadius: UISlider!
+    @IBOutlet weak var segmentsMonitoringMode: UISegmentedControl!
+    @IBOutlet weak var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func actionNavigationBack(_ sender: Any) {
+        _ = self.navigationController?.popViewController(animated: true)
+    }
+
+    @IBAction func actionNavBarBack(_ sender: Any) {
+        
+    }
+
+    @IBAction func actionSliderRadiusValueChanged(_ sender: Any) {
+    }
+}
+
