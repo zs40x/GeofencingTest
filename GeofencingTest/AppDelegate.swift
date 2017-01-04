87a92f16282ec48fcf8e944341e6c44d528631b0
Stefan Mehnert <StefanMehnert@gmail.com>
2017-01-04 19:24:11 +0100

    Added UserNotifcation framework, request notification permission on startup of the app in the appDelegate

--- GeofencingTest/AppDelegate.swift
+++ GeofencingTest/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {
+            (granted, error) in
+            NSLog("Failed to request authorization for localNotifications")
+        }
+        
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        
         return true
     }
 
