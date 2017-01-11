45d05678bbbd226f0f792dd752176c2b094ad974
Stefan Mehnert <StefanMehnert@gmail.com>
2017-01-11 05:21:19 +0100

    Moved geofencing monitoring to  seperate class

---
+++ GeofencingTest/Service/GeofenceMonitoring.swift
@@ -0,0 +1,48 @@
+//
+//  GeofenceMonitoring.swift
+//  GeofencingTest
+//
+//  Created by Stefan Mehnert on 11/01/2017.
+//  Copyright © 2017 Stefan Mehnert. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class GeofenceMonitoring {
+    
+    let locationManager: CLLocationManager
+    
+    init(locationManager: CLLocationManager) {
+        self.locationManager = locationManager
+    }
+    
+    
+    func startGeofenceMonitoring(_ geofence: Geofence) {
+        
+        guard CLLocationManager.isMonitoringAvailable(for: CLCircularRegion.self) else {
+            NSLog("Geofencing is not supported on this device!")
+            return
+        }
+        
+        guard CLLocationManager.authorizationStatus() == .authorizedAlways else {
+            NSLog("Geofence monitoring not possible if the app has no access to the location")
+            return
+        }
+        
+        locationManager.startMonitoring(for: makeRegion(geofence: geofence))
+        
+        NSLog("Now monitoring region for geofence: \(geofence)")
+    }
+    
+    
+    private func makeRegion(geofence: Geofence) -> CLCircularRegion {
+        
+        let region = CLCircularRegion(center: geofence.coordinate, radius: Double(geofence.radius), identifier: geofence.identifier)
+        
+        region.notifyOnEntry = (geofence.monitoringMode == .Entering)
+        region.notifyOnExit = !region.notifyOnEntry
+        
+        return region
+    }
+}
--- GeofencingTest/ViewControllers/MapViewController.swift
+++ GeofencingTest/ViewControllers/MapViewController.swift
@@ -132,33 +132,6 @@ class MapViewController: UIViewController {
             startGeofenceMonitoring(geofence)
         }
     }
-    
-    func startGeofenceMonitoring(_ geofence: Geofence) {
-            
-        guard CLLocationManager.isMonitoringAvailable(for: CLCircularRegion.self) else {
-            NSLog("Geofencing is not supported on this device!")
-            return
-        }
-            
-        guard CLLocationManager.authorizationStatus() == .authorizedAlways else {
-            NSLog("Geofence monitoring not possible if the app has no access to the location")
-            return
-        }
-            
-        locationManager.startMonitoring(for: makeRegion(geofence: geofence))
-            
-        NSLog("Now monitoring region for geofence: \(geofence)")
-    }
-    
-    private func makeRegion(geofence: Geofence) -> CLCircularRegion {
-        
-        let region = CLCircularRegion(center: geofence.coordinate, radius: Double(geofence.radius), identifier: geofence.identifier)
-        
-        region.notifyOnEntry = (geofence.monitoringMode == .Entering)
-        region.notifyOnExit = !region.notifyOnEntry
-        
-        return region
-    }
 }
 
 extension MapViewController: MKMapViewDelegate {
